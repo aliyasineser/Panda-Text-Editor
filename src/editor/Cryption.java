1e9e1e7d15ef978b93debdc11217ff72fa6be46c
Kaan-Ucar <kaanucar48@gmail.com>
2016-10-28 14:09:35 +0300

    Cryption.java ya password özelliği eklendi.

--- src/editor/Cryption.java
+++ src/editor/Cryption.java
@@ -16,79 +16,89 @@ public class Cryption {
     private final SecretKeySpec key;
     private final IvParameterSpec iv;
     private final byte [] message;
+    private final String password;
   
-    public Cryption(byte[] ivBytes, byte[] messageBytes) {
+    public Cryption(byte[] ivBytes, byte[] messageBytes, String strPassword) {
   	key = new SecretKeySpec(ivBytes, "AES");
       	iv = new IvParameterSpec(ivBytes);
       	message = messageBytes;
+        password = strPassword;
     }
     
-    public Cryption(byte[] messageBytes) {
+    public Cryption(byte[] messageBytes, String strPassword) {
         byte[] bytes = new byte[ivLength];
         new Random().nextBytes(bytes);
         
         key = new SecretKeySpec(bytes, "AES");
       	iv = new IvParameterSpec(bytes);
         message = messageBytes;
+        password = strPassword;
     }
     
     public IvParameterSpec getIv(){
         return iv;
     }
     
-    public static byte[] encryptFile(byte[] messageBytes) {
-        final int INT_SIZE = 4;
-        
-        Cryption crypt = new Cryption(messageBytes);
-        byte[] ivBytes = crypt.getIv().getIV();
-        byte[] lengthBytes = java.nio.ByteBuffer.allocate(INT_SIZE).putInt(ivBytes.length).array();
-        byte[] fileBytes;
-        
-        try{
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            baos.write(lengthBytes);
-            baos.write(ivBytes);
-            baos.write(crypt.encrypt());
-        
-            fileBytes = baos.toByteArray();
-            baos.close();
-        }
-        catch(Exception ex){
-            return null;
-        }
-        
-        return fileBytes;
+    public static byte[] encryptFile(byte[] fileBytes, String strPassword) {
+        return (new Cryption(fileBytes, strPassword)).encrypt();
     }
     
-    public static byte[] decryptFile(byte[] fileBytes) {
+    public static byte[] decryptFile(byte[] fileBytes, String strPassword) {
         final int INT_SIZE = 4;
         
-        byte[] lengthBytes = Arrays.copyOfRange(fileBytes, 0, INT_SIZE);
-        int length = java.nio.ByteBuffer.wrap(lengthBytes).getInt();
-        byte[] ivBytes = Arrays.copyOfRange(fileBytes, INT_SIZE, INT_SIZE + length);
-        byte[] messageBytes = Arrays.copyOfRange(fileBytes, INT_SIZE + length, fileBytes.length);
-        
-        return (new Cryption(ivBytes, messageBytes)).decrypt();
+        byte[] keyLengthBytes = Arrays.copyOfRange(fileBytes, 0, INT_SIZE);
+        int keyLength = java.nio.ByteBuffer.wrap(keyLengthBytes).getInt();
+            
+        byte[] keyBytes = Arrays.copyOfRange(fileBytes, INT_SIZE, INT_SIZE + keyLength);
+        return (new Cryption(keyBytes, fileBytes, strPassword)).decrypt();
     }
     
     public byte[] encrypt() {       
         try{
+            final int INT_SIZE = 4;
+            
             Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
             cipher.init(Cipher.ENCRYPT_MODE, key, iv);
             
-            return cipher.doFinal(message);
+            byte[] ivBytes = getIv().getIV();
+            byte[] ivLengthBytes = java.nio.ByteBuffer.allocate(INT_SIZE).putInt(ivBytes.length).array();
+            byte[] passwordBytes = cipher.doFinal(ByteArrayConverter.convertToByteArray(password));
+            byte[] passwordLengthBytes = java.nio.ByteBuffer.allocate(INT_SIZE).putInt(passwordBytes.length).array();
+            byte[] messageBytes = cipher.doFinal(message);
+                    
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            baos.write(ivLengthBytes);
+            baos.write(ivBytes);
+            baos.write(passwordLengthBytes);
+            baos.write(passwordBytes);
+            baos.write(messageBytes);
+        
+            byte [] fileBytes = baos.toByteArray();
+            baos.close();
+            
+            return fileBytes;
         }
         catch(Exception ex){
             return null;
         }
     }
 
-    public byte[] decrypt() {       
+    public byte[] decrypt() {
         try{
+            final int INT_SIZE = 4;
+            
             Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
             cipher.init(Cipher.DECRYPT_MODE, key, iv);
             
-            return cipher.doFinal(message);
+            int keyLength = getIv().getIV().length;
+            byte[] passwordLengthBytes = Arrays.copyOfRange(message, INT_SIZE + keyLength, 2*INT_SIZE + keyLength);
+            int passwordLength = java.nio.ByteBuffer.wrap(passwordLengthBytes).getInt();
+            byte[] passwordBytes = cipher.doFinal(Arrays.copyOfRange(message, 2*INT_SIZE + keyLength, 2*INT_SIZE + keyLength + passwordLength));
+            
+            if(!((String)(ByteArrayConverter.convertFromByteArray(passwordBytes))).equals(password))
+                return null;
+            
+            return cipher.doFinal(Arrays.copyOfRange(message, 2*INT_SIZE + keyLength + passwordLength, message.length));
         }
         catch(Exception ex){
             return null;
