40b11658b97d59fe0b80244ee3ae6fefe01cfa76
Habibe Yorulmaz <habibeyorulmaz@users.noreply.github.com>
2016-11-11 13:51:41 +0200

    added comments

--- src/editor/DirSave.java
+++ src/editor/DirSave.java
@@ -1,3 +1,8 @@
+/*
+ * This class saves the file as encrypted to given the directory 
+ *
+ */
+
 package editor;
 
 import java.io.File;
@@ -9,16 +14,32 @@ import java.io.IOException;
  * @author Kaan Ucar
  */
 public class DirSave{
+    /*This method saves the given file to a directory
+     *
+     * @param file given file that want to save
+     * @param object this object converted as a byte array
+     * @param password the password of given file
+     * @return save() call the wrapper save function
+     */
     public static boolean save(File file, Object object, String password){
         return save(file, ByteArrayConverter.convertToByteArray(object),password);
     }
     
+    /*This method encrypted the given file 
+     *
+     * @param file given file that want to save
+     * @param bytes uses for cryption
+     * @param password the password of given file
+     * @return true when successful saving
+     * @return false when unsuccessful saving
+     */
     private static boolean save(File file, byte[] bytes, String password){
         byte[] encryptedBytes = Cryption.encryptFile(bytes, password);
         if(encryptedBytes == null)
             return false;
         
         try{
+           // writting encrypted file to the directory path
             Files.write(Paths.get(file.getPath()), encryptedBytes);
         }
         catch(IOException ex){
