4352c9940cf3c4a98014b5a1a4f35c7dad455840
Kaan-Ucar <kaanucar48@gmail.com>
2016-11-14 09:52:35 +0200

    comments added

--- src/editor/DirSave.java
+++ src/editor/DirSave.java
@@ -5,17 +5,36 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
 
-/**
+/** Includes save functions to directory
  *
  * @author Kaan Ucar
  */
 public class DirSave {
-
+    /**
+     * Saves given object with encrypting by given password.
+     * Returns false if there is any problem.
+     * 
+     * @param file path
+     * @param object object
+     * @param password password
+     * 
+     * @return save statement
+     */
     public static boolean save(File file, Object object, String password) {
         
         return save(file, ByteArrayConverter.convertToByteArray(object), password);
     }
 
+    /**
+     * Saves given byte array with encrypting by given password.
+     * Returns false if there is any problem.
+     * 
+     * @param file path
+     * @param object object
+     * @param password password
+     * 
+     * @return save statement
+     */
     private static boolean save(File file, byte[] bytes, String password) {
         byte[] encryptedBytes = Cryption.encryptFile(bytes, password);
         if (encryptedBytes == null) {
