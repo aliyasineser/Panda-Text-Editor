b4c88e9bbc99496d7ad37c66cb3b5d5a8ac36614
habibeyorulmaz <habibeyorulmaz@gmail.com>
2016-11-18 14:55:09 +0300

    Revert "added comments"
    This reverts commit 40b11658b97d59fe0b80244ee3ae6fefe01cfa76.

--- src/editor/DirSave.java
+++ src/editor/DirSave.java
@@ -1,8 +1,3 @@
-/*
- * This class saves the file as encrypted to given the directory 
- *
- */
-
 package editor;
 
 import java.io.File;
@@ -16,25 +11,10 @@ import java.io.IOException;
  */
 <<<<<<< HEAD
 public class DirSave{
-    /*This method saves the given file to a directory
-     *
-     * @param file given file that want to save
-     * @param object this object converted as a byte array
-     * @param password the password of given file
-     * @return save() call the wrapper save function
-     */
     public static boolean save(File file, Object object, String password){
         return save(file, ByteArrayConverter.convertToByteArray(object),password);
     }
     
-    /*This method encrypted the given file 
-     *
-     * @param file given file that want to save
-     * @param bytes uses for cryption
-     * @param password the password of given file
-     * @return true when successful saving
-     * @return false when unsuccessful saving
-     */
     private static boolean save(File file, byte[] bytes, String password){
 =======
 public class DirSave {
@@ -52,7 +32,6 @@ public class DirSave {
 <<<<<<< HEAD
         
         try{
-           // writting encrypted file to the directory path
             Files.write(Paths.get(file.getPath()), encryptedBytes);
 =======
 >>>>>>> refs/remotes/origin/master
